28464aa3028b1e2e5f1f4c83036ea6da28f81a7b
mash <mashalonzo741@gmail.com>
2021-09-27 16:25:45 +0300

    Add the subsciber model

--- app/models.py
+++ app/models.py
@@ -92,3 +92,25 @@ class Comment(db.Model):
     def get_comment_id(self,id):
         comment = Comment.query.filter_by(id = id).first()
         return comment
+
+
+class Quote:
+    def __init__(self,id,author,quote):
+        self.id = id
+        self.author = author
+        self.quote = quote
+
+
+class Subscriber(db.Model):
+    __tablename__ = 'subscribers'
+
+    id = db.Column(db.Integer,primary_key = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+
+    def save_subscriber(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_all_mails(cls):
+        return Subscriber.query.order_by(Subscriber.id).all()
