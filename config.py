ff28999ae0c908b39c9242ba6db2d65ced5ecac3
mash <mashalonzo741@gmail.com>
2021-09-27 13:43:03 +0300

    Initialize the app

---
+++ config.py
@@ -0,0 +1,50 @@
+import os
+
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    # SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = 'Flask78WTF78Secret67Key'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mash:password@localhost/personal'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+class TestConfig(Config):
+    '''
+    Test  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mash:password@localhost/personal_test'
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    Debug = True
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig,
+    'test':TestConfig
+}
